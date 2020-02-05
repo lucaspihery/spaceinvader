51c018b1e6d5404d6afc59bc89a991c428353914
lucas <lucas.pihery@gmail.com>
2020-02-05 16:05:31 +0100

    Ennemi

--- src/main.c
+++ src/main.c
@@ -12,27 +12,24 @@
 #include "vt100.h"
 #include "serial.h"
 
-char my_ship[] = "<=^=>";
+int i = 0;
+int k = 0;
+uint8_t sens = 1;
+char vie = '3';
+uint8_t inp_clav;
+uint8_t xv = 40;
+uint8_t yv = 20;
+uint8_t xa = 0;
+uint8_t ya = 3;
+char ship[] = "<=^=>";
 char bad_ship1[] = "<VV>";
 char bad_ship2 = 'o';
 char bad_ship3 = 'o';
-int a;
-int i = 0;
-int x=38;
-int xm=38;
-int sens=1;
-/*
- **===========================================================================
- **
- **  Abstract: main program
- **
- **===========================================================================
- */
+
 int main(void) {
 
 	serial_init(115200);
 	vt100_clear_screen();
-
 	vt100_move(35, 3);
 	serial_puts("SPACE INVADERS");
 	vt100_move(36, 5);
@@ -43,102 +40,101 @@ int main(void) {
 	serial_puts("<==> : 250 Pts");
 	vt100_move(35, 12);
 	serial_puts("<VV> : 100 Pts");
-	vt100_move(35,14);
+	vt100_move(35, 14);
 	serial_puts("<mm> : 50 Pts");
-	vt100_move(31,20);
+	vt100_move(31, 20);
 	serial_puts("Press <space> to start !");
 
-	while (a != 32) {
-		a = serial_get_last_char();
-	}
-
-	vt100_clear_screen();
-	vt100_move(x,20);
-	serial_puts(my_ship);
-	vt100_move(20,24);
-	serial_puts("Vies : 3");
-	vt100_move(50,24);
-	serial_puts("Points : 0");
-
-	while (1)
+	while (inp_clav != 32) /*32 ASCII de l'espace*/
 	{
+		inp_clav = serial_get_last_char();
+	}
+	vt100_clear_screen();
+	vt100_move(xv, 20);
+	serial_puts(ship);
+
+	while (1) {
 		i++;
 
-		if(i == 100)
-		{
-			if(sens == 1)
-					{
-						vt100_move(xm,10);
-						serial_puts("    ");
-						xm += 1;
-						vt100_move(xm,10);
-						serial_puts(bad_ship1);
-						if(xm == 79)
-						{
-							sens=0;
-						}
-					}
-					if(sens == 0)
-					{
-						vt100_move(xm,10);
-						serial_puts("    ");
-						xm -= 1;
-						vt100_move(xm,10);
-						serial_puts(bad_ship1);
-						if(xm == 1)
-						{
-							sens=1;
-						}
-					}
-					i=0;
-		}
+		vt100_move(37, 1);
+		serial_puts("SPACE INVADERS");
+		vt100_move(1, 1);
+		serial_puts("SCORE: ");
+		vt100_move(65, 1);
+		serial_puts("VIES: ");
+		vt100_move(75, 1);
+		serial_putchar(vie);
+		inp_clav = serial_get_last_char();
 
-
-
-
-	a = serial_get_last_char();
-
-
-	if (a == 'd')
-	{
-		vt100_move(x,20);
-		serial_puts("     ");
-		if(x != 76)
-		{
-			x += 1;
-		}
-		vt100_move(x,20);
-		serial_puts(my_ship);
-	}
-	if (a == 'q')
-		{
-			vt100_move(x,20);
-			serial_puts("     ");
-			if(x != 1)
-			{
-				x -= 1;
+		if (inp_clav == 'd') {
+			vt100_move(xv, yv);
+			serial_puts("    ");
+			if (xv != 76) {
+				xv += 1;
 			}
-			vt100_move(x,20);
-			serial_puts(my_ship);
+			vt100_move(xv, yv);
+			serial_puts(ship);
+
 		}
 
+		if (inp_clav == 'q') {
+			vt100_move(xv, yv);
+			serial_puts("     ");
+			if (xv != 1) {
+				xv -= 1;
+			}
+			vt100_move(xv, 20);
+			serial_puts(ship);
+		}
+
+		/*alien*/
+
+		if (i == 15) {
+			if (sens == 1) {
+				vt100_move(xa, ya);
+				serial_puts("    ");
+				xa += 1;
+				vt100_move(xa, ya);
+				serial_puts(bad_ship1);
+				if (xa == 78) {
+					vt100_move(xa, ya);
+					serial_puts("    ");
+					ya += 1;
+					sens = 0;
+				}
+
+			}
+			if (sens == 0) {
+				vt100_move(xa, ya);
+				serial_puts("    ");
+				xa -= 1;
+				vt100_move(xa, ya);
+				serial_puts(bad_ship1);
+				if (xa == 1) {
+					vt100_move(xa, ya);
+					serial_puts("    ");
+					ya += 1;
+					sens = 1;
+				}
+
+				if (xa == 80) {
+					vt100_move(xa, ya);
+					serial_puts("    ");
+					xa = 0;
+					ya += 1;
+				}
+
+			}
+			i = 0;
+		}
+	}
 }
-}
-/*
- * Callback used by stm32f4_discovery_audio_codec.c.
- * Refer to stm32f4_discovery_audio_codec.h for more info.
- */
+
 void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size) {
-	/* TODO, implement your code here */
 	return;
 }
 
-/*
- * Callback used by stm324xg_eval_audio_codec.c.
- * Refer to stm324xg_eval_audio_codec.h for more info.
- */
 uint16_t EVAL_AUDIO_GetSampleCallBack(void) {
-	/* TODO, implement your code here */
 	return -1;
 }
 
