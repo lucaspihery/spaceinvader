4949ad0fb52ca6806a052c897f075bedb60ea884
lucas <lucas.pihery@gmail.com>
2020-02-05 17:03:34 +0100

    Logo

--- src/main.c
+++ src/main.c
@@ -12,8 +12,6 @@
 #include "vt100.h"
 #include "serial.h"
 
-int i = 0;
-int k = 0;
 uint8_t sens = 1;
 char vie = '3';
 uint8_t inp_clav;
@@ -21,6 +19,10 @@ uint8_t xv = 40;
 uint8_t yv = 20;
 uint8_t xa = 0;
 uint8_t ya = 3;
+uint8_t missile_lance = 0;
+uint8_t xm=40;
+uint8_t ym=21;
+char missile = '|';
 char ship[] = "<=^=>";
 char bad_ship1[] = "<VV>";
 char bad_ship2 = 'o';
@@ -28,19 +30,34 @@ char bad_ship3 = 'o';
 
 int main(void) {
 
+	int i = 0;
+	int h = 0;
+
 	serial_init(115200);
 	vt100_clear_screen();
-	vt100_move(35, 3);
-	serial_puts("SPACE INVADERS");
-	vt100_move(36, 5);
-	serial_puts("LUCAS PIHERY");
-	vt100_move(35, 8);
+	vt100_move(30, 1);
+	serial_puts("    ##          ##");
+	vt100_move(30, 2);
+	serial_puts("      ##      ##");
+	vt100_move(30, 3);
+	serial_puts("    ##############");
+	vt100_move(30, 4);
+	serial_puts("  ####  ######  ####");
+	vt100_move(30, 5);
+	serial_puts("######################");
+	vt100_move(30, 6);
+	serial_puts("##  ##############  ##");
+	vt100_move(30, 7);
+	serial_puts("##  ##          ##  ##");
+	vt100_move(30, 8);
+	serial_puts("      ####  ####");
+	vt100_move(35, 9);
 	serial_puts("<TT> : 500 Pts");
-	vt100_move(35, 10);
+	vt100_move(35, 11);
 	serial_puts("<==> : 250 Pts");
-	vt100_move(35, 12);
+	vt100_move(35, 13);
 	serial_puts("<VV> : 100 Pts");
-	vt100_move(35, 14);
+	vt100_move(35, 15);
 	serial_puts("<mm> : 50 Pts");
 	vt100_move(31, 20);
 	serial_puts("Press <space> to start !");
@@ -88,6 +105,33 @@ int main(void) {
 		}
 
 		/*alien*/
+		if (inp_clav == 32)
+		{
+			missile_lance = 1;
+		}
+		if (missile_lance == 1)
+		{
+			h++;
+
+			if (ym != 3 && h == 20)
+			{
+				vt100_move(xm, ym);
+				serial_putchar(' ');
+				ym -= 1;
+				vt100_move(xm, ym);
+				serial_putchar(missile);
+				h = 0;
+
+				if (ym == 3)
+				{
+					vt100_move(xm, 3);
+					serial_putchar(' ');
+					ym = 21;
+					xm = xv+2;
+					missile_lance = 0;
+				}
+			}
+		}
 
 		if (i == 15) {
 			if (sens == 1) {
